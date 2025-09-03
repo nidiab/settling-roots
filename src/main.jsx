52c2f55a7e19c9ba5b0d9baf4947f910e0fba82d
Nidia <nidiabatista@gmail.com>
2025-09-03 13:34:12 +0200

    Fix site

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,22 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
+// Initialize Google Analytics (GA4) if VITE_GA_ID is set
+const GA_ID = import.meta.env.VITE_GA_ID
+if (GA_ID) {
+  // load gtag.js
+  const s = document.createElement('script')
+  s.async = true
+  s.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
+  document.head.appendChild(s)
+  // init gtag
+  window.dataLayer = window.dataLayer || []
+  function gtag(){ window.dataLayer.push(arguments) }
+  window.gtag = gtag
+  gtag('js', new Date())
+  gtag('config', GA_ID)
+}
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
